75783b862670dae248dd6e250bfa5459aea52b93
Ethan <kim76744dev@gmail.com>
2024-05-18 19:22:38 +0900

    feat: 생성, 수정 시간 추가

--- src/main/kotlin/backend/itracker/crawl/domain/BaseEntity.kt
+++ src/main/kotlin/backend/itracker/crawl/domain/BaseEntity.kt
@@ -1,15 +1,30 @@
 package backend.itracker.crawl.domain
 
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
 abstract class BaseEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0L
+    val id: Long = 0L,
+
+    @Column(updatable = false, nullable = false)
+    @CreatedDate
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    val updatedAt: LocalDateTime = LocalDateTime.now(),
 ) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
