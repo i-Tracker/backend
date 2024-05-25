cd23d6a7baddf6f63d069fdf52bfdfd10ce03849
Ethan <kim76744dev@gmail.com>
2024-05-25 15:20:04 +0900

    아이패드 크롤링 기능 구현 (#36)
    feat: 아이패드 크롤링 기능 구현

---
+++ src/main/kotlin/backend/itracker/crawl/ipad/domain/IpadCategory.kt
@@ -0,0 +1,8 @@
+package backend.itracker.crawl.ipad.domain
+
+enum class IpadCategory {
+    I_PAD,
+    I_PAD_PRO,
+    I_PAD_AIR,
+    I_PAD_MINI
+}
--- src/main/kotlin/backend/itracker/crawl/service/CrawlService.kt
+++ src/main/kotlin/backend/itracker/crawl/service/CrawlService.kt
@@ -1,10 +1,12 @@
 package backend.itracker.crawl.service
 
+import backend.itracker.crawl.ipad.domain.Ipad
 import backend.itracker.crawl.macbook.domain.Macbook
 import backend.itracker.crawl.service.mapper.CrawlMapper
 import backend.itracker.crawl.service.util.Crawler
 import org.springframework.stereotype.Component
 
+private const val CRAWL_URL = "https://pages.coupang.com/p/"
 
 @Component
 class CrawlService(
@@ -12,9 +14,19 @@ class CrawlService(
     private val crawler: Crawler
 ) {
 
-    fun crawlMacbook(crawlTargetCategory: CrawlTargetCategory): List<Macbook> {
-        val url = "https://pages.coupang.com/p/${crawlTargetCategory.categoryId}"
+    fun crawlMacbook(): List<Macbook> {
+        val url = getCrawlUrl(CrawlTargetCategory.MACBOOK)
         val products = crawler.crawl(url)
         return crawlMapper.toMacbook(products)
+    }
+
+    fun crawlIpad(): List<Ipad> {
+        val url = getCrawlUrl(CrawlTargetCategory.IPAD)
+        val products = crawler.crawl(url)
+        return crawlMapper.toIpad(products)
+    }
+
+    private fun getCrawlUrl(category: CrawlTargetCategory): String {
+        return "$CRAWL_URL${category.categoryId}"
     }
 }
