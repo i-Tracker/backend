1864790e8a983fd00243010334d479d8d79d224d
Ethan <kim76744dev@gmail.com>
2024-05-26 23:03:12 +0900

    feat: 비동기 추가

---
+++ src/main/kotlin/backend/itracker/schedule/config/AsyncConfig.kt
@@ -0,0 +1,8 @@
+package backend.itracker.schedule.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+
+@EnableAsync
+@Configuration
+class AsyncConfig
--- src/main/kotlin/backend/itracker/schedule/controller/ScheduleController.kt
+++ src/main/kotlin/backend/itracker/schedule/controller/ScheduleController.kt
@@ -2,7 +2,7 @@ package backend.itracker.schedule.controller
 
 import backend.itracker.crawl.service.CrawlTargetCategory
 import backend.itracker.schedule.service.SchedulerService
-import org.springframework.http.ResponseEntity
+import org.springframework.scheduling.annotation.Async
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -11,16 +11,14 @@ import org.springframework.web.bind.annotation.RestController
 class ScheduleController(
     private val schedulerService: SchedulerService
 ) {
-
+    @Async
     @PostMapping("/api/v1/schedule/update")
-    fun manualCrawl(@RequestParam category: CrawlTargetCategory): ResponseEntity<Unit> {
+    fun manualCrawl(@RequestParam category: CrawlTargetCategory) {
         when (category) {
             CrawlTargetCategory.MACBOOK -> schedulerService.crawlMacbook()
             CrawlTargetCategory.IPAD -> schedulerService.crawlIpad()
             CrawlTargetCategory.APPLE_WATCH -> schedulerService.crawlAppleWatch()
-            else -> return ResponseEntity.badRequest().build()
+            else -> throw IllegalArgumentException("수동 업데이트를 지원하지 않는 카테고리 입니다.")
         }
-
-        return ResponseEntity.ok().build()
     }
 }
