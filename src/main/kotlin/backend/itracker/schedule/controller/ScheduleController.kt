ff1493dc251b881a6e67b7571acec68be745a648
Ethan <kim76744dev@gmail.com>
2024-05-26 23:48:20 +0900

    5/26 배포 작업 (#43)

---
+++ src/main/kotlin/backend/itracker/schedule/controller/ScheduleController.kt
@@ -0,0 +1,24 @@
+package backend.itracker.schedule.controller
+
+import backend.itracker.crawl.service.CrawlTargetCategory
+import backend.itracker.schedule.service.SchedulerService
+import org.springframework.scheduling.annotation.Async
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ScheduleController(
+    private val schedulerService: SchedulerService
+) {
+    @Async
+    @PostMapping("/api/v1/schedule/update")
+    fun manualCrawl(@RequestParam category: CrawlTargetCategory) {
+        when (category) {
+            CrawlTargetCategory.MACBOOK -> schedulerService.crawlMacbook()
+            CrawlTargetCategory.IPAD -> schedulerService.crawlIpad()
+            CrawlTargetCategory.APPLE_WATCH -> schedulerService.crawlAppleWatch()
+            else -> throw IllegalArgumentException("수동 업데이트를 지원하지 않는 카테고리 입니다.")
+        }
+    }
+}
