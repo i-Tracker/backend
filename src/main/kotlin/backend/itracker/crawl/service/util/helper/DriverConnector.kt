86fa1962f629a209fde3d52b31c97182e9d699c5
Ethan <kim76744dev@gmail.com>
2024-05-27 12:25:29 +0900

    Driver 경로 추가 (#45)
    feat: driver 경로 추가

--- src/main/kotlin/backend/itracker/crawl/service/util/helper/DriverConnector.kt
+++ src/main/kotlin/backend/itracker/crawl/service/util/helper/DriverConnector.kt
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.remote.RemoteWebDriver
 import org.openqa.selenium.support.ui.FluentWait
 import org.openqa.selenium.support.ui.WebDriverWait
+import org.springframework.beans.factory.annotation.Value
 import java.time.Duration
 
 private const val USER_AGENT =
@@ -14,15 +15,24 @@ private const val DEFAULT_WAIT_TIME = 5L
 
 class DriverConnector {
 
+    @Value("\${driver.path}")
+    lateinit var driverPath: String
+
     fun getDriver(): RemoteWebDriver {
         val chromeOptions = ChromeOptions()
         chromeOptions.addArguments(USER_AGENT)
         chromeOptions.addArguments("--headless")
+        chromeOptions.addArguments("--remote-debugging-pipe")
+        chromeOptions.addArguments("enable-automation")
         chromeOptions.addArguments("--disable-gpu")
         chromeOptions.addArguments("--no-sandbox")
         chromeOptions.addArguments("--disable-dev-shm-usage")
         chromeOptions.addArguments("--disable-notifications")
         chromeOptions.addArguments("--disable-extensions")
+
+        if (System.getProperty("os.name").lowercase().contains("linux")) {
+            chromeOptions.setBinary(driverPath)
+        }
 
         return ChromeDriver(chromeOptions)
     }
