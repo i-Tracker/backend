a1b5a44b754e74a31fbf7d8c6e10a2e08472d218
Ethan <kim76744dev@gmail.com>
2024-05-28 13:31:56 +0900

    셀레니움 RemoteDriver 로 변경 (#47)
    * refactor: firefox로 변경
    
    * refactor: firefox standalone을 사용하기 위해 remote driver 설정
    
    * test: 테스트 설정파일 추가

--- src/main/kotlin/backend/itracker/crawl/service/util/helper/DriverConnector.kt
+++ src/main/kotlin/backend/itracker/crawl/service/util/helper/DriverConnector.kt
@@ -1,40 +1,33 @@
 package backend.itracker.crawl.service.util.helper
 
 import org.openqa.selenium.WebDriver
-import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.firefox.FirefoxOptions
 import org.openqa.selenium.remote.RemoteWebDriver
 import org.openqa.selenium.support.ui.FluentWait
 import org.openqa.selenium.support.ui.WebDriverWait
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.net.URL
 import java.time.Duration
 
 private const val USER_AGENT =
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
 private const val DEFAULT_WAIT_TIME = 5L
 
+@Component
 class DriverConnector {
 
-    @Value("\${driver.path}")
-    lateinit var driverPath: String
+    @Value("\${selenium.driver-url}")
+    private lateinit var driverUrl: String
 
     fun getDriver(): RemoteWebDriver {
-        val chromeOptions = ChromeOptions()
-        chromeOptions.addArguments(USER_AGENT)
-        chromeOptions.addArguments("--headless")
-        chromeOptions.addArguments("--remote-debugging-pipe")
-        chromeOptions.addArguments("enable-automation")
-        chromeOptions.addArguments("--disable-gpu")
-        chromeOptions.addArguments("--no-sandbox")
-        chromeOptions.addArguments("--disable-dev-shm-usage")
-        chromeOptions.addArguments("--disable-notifications")
-        chromeOptions.addArguments("--disable-extensions")
+        val firefoxOptions = FirefoxOptions()
+        firefoxOptions.addArguments(USER_AGENT)
+        firefoxOptions.addArguments("--headless")
+        firefoxOptions.addArguments("--no-sandbox")
+        firefoxOptions.setCapability("acceptInsecureCerts", true)
 
-        if (System.getProperty("os.name").lowercase().contains("linux")) {
-            chromeOptions.setBinary(driverPath)
-        }
-
-        return ChromeDriver(chromeOptions)
+        return RemoteWebDriver(URL(driverUrl), firefoxOptions)
     }
 
     fun getWaiter(driver: RemoteWebDriver): FluentWait<out WebDriver> {
