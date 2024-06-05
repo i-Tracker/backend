27cf35ee89e3d794820b85fb7fb1cf733cadd64d
Ethan <kim76744dev@gmail.com>
2024-06-05 23:04:11 +0900

    6/5 배포작업 (#65)

---
+++ src/main/kotlin/backend/itracker/tracker/service/service/CoupangApiClient.kt
@@ -0,0 +1,50 @@
+package backend.itracker.tracker.service.service
+
+import backend.itracker.tracker.service.common.HmacGenerator
+import backend.itracker.tracker.service.request.CoupangLinkRequest
+import backend.itracker.tracker.service.response.CoupangLinkResponse
+import backend.itracker.tracker.service.response.Deeplink
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+
+private const val REQUEST_METHOD = "POST"
+private const val DOMAIN = "https://api-gateway.coupang.com"
+private const val URL = "/v2/providers/affiliate_open_api/apis/openapi/v1/deeplink"
+
+private const val APPLICATION_JSON = "application/json"
+private const val UTF_8 = "UTF-8"
+private const val AUTHORIZATION = "Authorization"
+private const val CONTENT_TYPE = "Content-Type"
+private const val CONTENT_ENCODING = "Content-Encoding"
+
+@Service
+class CoupangApiClient(
+    val hmacGenerator: HmacGenerator,
+    val restTemplate: RestTemplate
+) {
+
+    @Value("\${coupang.access-key}")
+    private lateinit var accessKey: String
+
+    @Value("\${coupang.secret-key}")
+    private lateinit var secretKey: String
+
+    fun issueCoupangLinks(originLinks: List<String>): List<Deeplink> {
+        val authorization = hmacGenerator.generate(REQUEST_METHOD, URL, secretKey, accessKey)
+        val httpHeaders = HttpHeaders().apply {
+            set(AUTHORIZATION, authorization)
+            set(CONTENT_TYPE, APPLICATION_JSON)
+            set(CONTENT_ENCODING, UTF_8)
+        }
+        val body = ObjectMapper().writeValueAsString(CoupangLinkRequest(originLinks))
+
+        val request = HttpEntity(body, httpHeaders)
+        val response = restTemplate.postForEntity("$DOMAIN$URL", request, CoupangLinkResponse::class.java)
+        return response.body?.data ?: throw IllegalStateException("Deeplink가 생성중에 오류가 발생했습니다.")
+    }
+}
+
