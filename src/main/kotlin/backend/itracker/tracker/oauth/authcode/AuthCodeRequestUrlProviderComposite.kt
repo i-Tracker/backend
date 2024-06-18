623ce5237becd8212d4e7be1a3b8864196122073
Ethan <kim76744dev@gmail.com>
2024-06-18 23:43:01 +0900

    6/18 배포 작업 (#86)

--- src/main/kotlin/backend/itracker/tracker/oauth/authcode/AuthCodeRequestUrlProviderComposite.kt
+++ src/main/kotlin/backend/itracker/tracker/oauth/authcode/AuthCodeRequestUrlProviderComposite.kt
@@ -1,6 +1,7 @@
 package backend.itracker.tracker.oauth.authcode
 
 import backend.itracker.tracker.oauth.OauthServerType
+import backend.itracker.tracker.oauth.RedirectType
 import org.springframework.stereotype.Component
 
 @Component
@@ -10,6 +11,6 @@ class AuthCodeRequestUrlProviderComposite(
 
     private val mappers: Map<OauthServerType, AuthCodeRequestUrlProvider> = providers.associateBy { it.supports() }
 
-    fun provide(oauthServerType: OauthServerType): String = mappers[oauthServerType]?.provide()
+    fun provide(oauthServerType: OauthServerType, redirectType: RedirectType): String = mappers[oauthServerType]?.provide(redirectType)
         ?: throw IllegalStateException("지원하지 않는 OauthServerType 입니다. oauthServerType=$oauthServerType")
 }
--- src/main/kotlin/backend/itracker/tracker/oauth/client/OauthMemberClientComposite.kt
+++ src/main/kotlin/backend/itracker/tracker/oauth/client/OauthMemberClientComposite.kt
@@ -1,6 +1,7 @@
 package backend.itracker.tracker.oauth.client
 
 import backend.itracker.tracker.oauth.OauthServerType
+import backend.itracker.tracker.oauth.RedirectType
 import org.springframework.stereotype.Component
 
 @Component
@@ -10,6 +11,6 @@ class OauthMemberClientComposite(
 
     private val mappers: Map<OauthServerType, OauthMemberClient> = clients.associateBy { it.supports() }
 
-    fun fetch(oauthServerType: OauthServerType, code: String) = mappers[oauthServerType]?.fetch(code)
+    fun fetch(oauthServerType: OauthServerType, code: String, redirectType: RedirectType) = mappers[oauthServerType]?.fetch(code, redirectType)
         ?: throw IllegalStateException("지원하지 않는 소셜 로그인 타입 입니다. oauthServerType=$oauthServerType")
 }
