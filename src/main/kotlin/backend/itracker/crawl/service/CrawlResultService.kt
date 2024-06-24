0eabedcd63d6f57f480fff9bc8f15ffa8e0da267
Ethan <kim76744dev@gmail.com>
2024-06-24 17:56:14 +0900

    fix: 오늘 하루 동안 검색하도록 변경

--- src/main/kotlin/backend/itracker/crawl/service/CrawlResultService.kt
+++ src/main/kotlin/backend/itracker/crawl/service/CrawlResultService.kt
@@ -13,7 +13,7 @@ class CrawlResultService(
 ) {
 
     fun updateCrawlResult(crawlCategory: CrawlTargetCategory) {
-        val today = LocalDateTime.now()
+        val today = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0)
         val results = crawlResultRepository.findByCreatedAtBetween(today, today.plusDays(1L))
 
         if (results.isEmpty()) {
