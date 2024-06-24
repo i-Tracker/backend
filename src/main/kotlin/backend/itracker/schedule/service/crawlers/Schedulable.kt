7c040fd249d725699378e4ef8d8da4f0d51e137c
Ethan <kim76744dev@gmail.com>
2024-06-24 17:49:02 +0900

    refactor: scheduler 패키지 변경

---
+++ src/main/kotlin/backend/itracker/schedule/service/crawlers/Schedulable.kt
@@ -0,0 +1,16 @@
+package backend.itracker.schedule.service.crawlers
+
+import backend.itracker.crawl.service.CrawlResultService
+import backend.itracker.crawl.service.CrawlService
+import backend.itracker.crawl.service.CrawlTargetCategory
+
+abstract class Schedulable(
+    private val crawlResultService: CrawlResultService,
+    private val crawlService: CrawlService,
+) {
+
+    abstract fun supports(): CrawlTargetCategory
+
+    abstract fun doSchedule()
+}
+
