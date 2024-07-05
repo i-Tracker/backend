b461124366a8c4a34401eff7ef89ced4b5552bde
Ethan <kim76744dev@gmail.com>
2024-07-05 12:38:20 +0900

    feat: favoritecount 조회 메서드 구현

--- src/main/kotlin/backend/itracker/tracker/member/service/FavoriteService.kt
+++ src/main/kotlin/backend/itracker/tracker/member/service/FavoriteService.kt
@@ -1,10 +1,12 @@
 package backend.itracker.tracker.member.service
 
 import backend.itracker.tracker.member.domain.Favorite
+import backend.itracker.tracker.member.domain.FavoriteProduct
 import backend.itracker.tracker.member.domain.Member
 import backend.itracker.tracker.member.domain.repository.FavoriteRepository
 import backend.itracker.tracker.member.service.handler.FavoriteComposite
 import backend.itracker.tracker.member.service.handler.response.CommonFavoriteProductModel
+import backend.itracker.tracker.member.service.vo.FavoriteCount
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
@@ -42,5 +44,17 @@ class FavoriteService(
             .reversed().toList()
 
         return PageImpl(productsContents, pageFavorites.pageable, productsContents.size.toLong())
+    }
+
+    @Transactional(readOnly = true)
+    fun findFavoritesCountByFavoriteProduct(
+        favoriteProduct: FavoriteProduct
+    ): FavoriteCount {
+        val count = favoriteRepository.findCountByProduct(favoriteProduct)
+        return FavoriteCount(
+            productId = favoriteProduct.productId,
+            category = favoriteProduct.productCategory,
+            count = count
+        )
     }
 }
