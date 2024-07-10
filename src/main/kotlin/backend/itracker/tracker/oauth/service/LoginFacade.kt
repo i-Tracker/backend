5a504c8ab018e01e0040c3691f96a62a5cfe4259
Ethan <kim76744dev@gmail.com>
2024-07-10 14:13:58 +0900

    fix: 로그인 롤백

--- src/main/kotlin/backend/itracker/tracker/oauth/service/LoginFacade.kt
+++ src/main/kotlin/backend/itracker/tracker/oauth/service/LoginFacade.kt
@@ -14,17 +14,7 @@ class LoginFacade(
         authCode: String,
         redirectType: RedirectType
     ): String {
-        val member = oauthService.login(oauthServerType, authCode, RedirectType.LOCAL)
-
-        return oauthService.issueToken(member)
-    }
-
-    fun firstLogin(
-        oauthServerType: OauthServerType,
-        authCode: String,
-        redirectType: RedirectType
-    ): String {
-        val member = oauthService.firstLogin(oauthServerType, authCode, RedirectType.LOCAL)
+        val member = oauthService.fetchMember(oauthServerType, authCode, RedirectType.LOCAL)
         oauthService.register(member)
 
         return oauthService.issueToken(member)
