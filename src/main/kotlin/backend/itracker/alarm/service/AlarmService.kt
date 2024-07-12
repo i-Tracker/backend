f8f2a8cd299817c1b88f000d010be687a6357d7e
Ethan <kim76744dev@gmail.com>
2024-07-12 15:56:21 +0900

    7/12 배포 작업

--- src/main/kotlin/backend/itracker/alarm/service/AlarmService.kt
+++ src/main/kotlin/backend/itracker/alarm/service/AlarmService.kt
@@ -2,12 +2,13 @@ package backend.itracker.alarm.service
 
 import backend.itracker.alarm.infra.slack.Color
 import backend.itracker.alarm.infra.slack.SlackClient
+import backend.itracker.alarm.service.event.FailedNotificationEvent
 import backend.itracker.crawl.service.CrawlResultService
 import backend.itracker.schedule.service.event.CrawlEndEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
-private const val CRAWL_RESULT_TITLE = "[ 크롤링 결과 ]"
+private const val CRAWL_RESULT_TITLE = "*[ 크롤링 결과 ]*"
 
 @Component
 class AlarmService (
@@ -30,5 +31,17 @@ class AlarmService (
             true -> slackClient.sendNotification(CRAWL_RESULT_TITLE, Color.RED, messages)
             false -> slackClient.sendNotification(CRAWL_RESULT_TITLE, Color.GREEN, messages)
         }
+    }
+
+    @EventListener(FailedNotificationEvent::class)
+    fun alarmNotificationFailEvent(event: FailedNotificationEvent) {
+        slackClient.sendNotification(
+            """
+            |*[ 알림 전송 실패 ]*
+            |*실패 사유* : ${event.getCause()}
+            """.trimMargin(),
+            Color.RED,
+            event.getMessages()
+        )
     }
 }
