c4d2a6c67ddd9955bf5754c35e0eba33af19c315
Ethan <kim76744dev@gmail.com>
2024-07-13 23:08:11 +0900

    7/13 배포 작업 (#136)

---
+++ src/main/kotlin/backend/itracker/alarm/service/event/SuccessReservationOfNotificationEvent.kt
@@ -0,0 +1,15 @@
+package backend.itracker.alarm.service.event
+
+import java.time.LocalDateTime
+
+
+interface SuccessReservationOfNotificationEvent {
+
+    fun reservationTime(): LocalDateTime
+
+    fun successMessageCount(): Int
+
+    fun point(): Float
+
+    fun balance(): Float
+}
